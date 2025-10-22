592b880629551251e59a634dac9d4ea54ca24fc4
Jean <jean.agudelo@udea.edu.co>
2025-10-22 12:43:23 -0500

    realizo la clase de cancion
    Signed-off-by: Jean <jean.agudelo@udea.edu.co>

--- Codigo/desafio_2/cancion.h
+++ Codigo/desafio_2/cancion.h
@@ -1,10 +1,37 @@
 #ifndef CANCION_H
 #define CANCION_H
+#include <iostream>
+#include <string>
+using namespace std;
+#include "creditos.h"
 
 class cancion
 {
+private:
+    string nombre;
+    int id;
+    float duracion;
+    string ruta;
+    int reproducciones;
+    creditos *creditos;
+    int numCreditos;
+
 public:
     cancion();
+    cancion(string nom,int id, float dur,string rut);
+    cancion(const Cancion &copia);
+
+    ~Cancion();
+
+    void agregarCredito(const creditos &c);
+    void reproducir(int calidad) const;
+    void mostrar() const;
+
+    int getId() const;
+    string getNombre() const;
+    float getDuracion() const;
+    int getReproducciones() const;
+    string Ruta() const;
 };
 
 #endif // CANCION_H
