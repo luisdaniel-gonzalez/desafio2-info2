11752caa82760ea4ad7a1a15c87c3a638632def9
Jean <jean.agudelo@udea.edu.co>
2025-10-22 12:46:56 -0500

    realizo la clase de album
    Signed-off-by: Jean <jean.agudelo@udea.edu.co>

--- Codigo/desafio_2/album.h
+++ Codigo/desafio_2/album.h
@@ -1,10 +1,34 @@
 #ifndef ALBUM_H
 #define ALBUM_H
+#include <iostream>
+#include <string>
 
+using namespace std;
+#include "cancion.h"
 class album
 {
+private:
+    string nombre;
+    int codigo;
+    string fechaDeLanzamiento;
+    string sello;
+    string rutaPortada;
+    string genero[4];
+    int numGeneros;
+    float puntuacion;
+    float duracionTotal;
+    cancion *canciones;
+    int numcanciones;
+
 public:
     album();
+    album(string nom,int cod, string fecha, string sello, string portada);
+    album(const album &copia);
+
+    ~Album();
+
+    void agregarGenero(const string &g);
+    void agregarCancion(const cancion &c);
 };
 
 #endif // ALBUM_H
