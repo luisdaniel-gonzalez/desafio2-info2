4015321fd5e8271317ff31a6e126f88c74fd526e
Jean <jean.agudelo@udea.edu.co>
2025-10-22 12:27:19 -0500

    utilice el diagrama previamente construido para poder crear las clase, empiezo por los creditos
    Signed-off-by: Jean <jean.agudelo@udea.edu.co>

--- Codigo/desafio_2/creditos.h
+++ Codigo/desafio_2/creditos.h
@@ -1,10 +1,33 @@
 #ifndef CREDITOS_H
 #define CREDITOS_H
+#include <string>
+using namespace std;
 
 class creditos
 {
+private:
+    string nombre;
+    string apellido;
+    string sello;
+    string rol;
+
 public:
     creditos();
+    creditos(string nom, string ape, string cod, string rol);
+    creditos(const creditos &c);
+
+    ~Creditos();
+
+    void mostrar() const;
+
+    string getNombre() const;
+    string getApellido() const;
+    string getSello() const;
+    string getRol() const;
+    void setNombre(const string &n);
+    void setApellido(const string &a);
+    void setSello(const string &s);
+    void setRol(const string &r);
 };
 
 #endif // CREDITOS_H
