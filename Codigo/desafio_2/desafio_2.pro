23e65f348e955d732d2e90e060768c46328142dc
LUIS DANIEL GONZALEZ CORREA <luisdaniel.gonzalez@udea.edu.co>
2025-10-21 21:42:22 -0500

    Hago menú basico para el main
    Signed-off-by: LUIS DANIEL GONZALEZ CORREA <luisdaniel.gonzalez@udea.edu.co>

--- Codigo/desafio_2/desafio_2.pro
+++ Codigo/desafio_2/desafio_2.pro
@@ -5,15 +5,25 @@ CONFIG -= qt
 
 SOURCES += \
         main.cpp \
+        plataforma.cpp \
         plataforma/plataforma.cpp \
+        publicidad.cpp \
         publicidad/publicidad.cpp \
+        usuario.cpp \
         usuario/usuario.cpp \
+        usuarioEstandar.cpp \
         usuarioEstandar/usuarioEstandar.cpp \
+        usuarioPremium.cpp \
         usuarioPremium/usuarioPremium.cpp
 
 HEADERS += \
+    plataforma.h \
     plataforma/plataforma.h \
+    publicidad.h \
     publicidad/publicidad.h \
+    usuario.h \
     usuario/usuario.h \
+    usuarioEstandar.h \
     usuarioEstandar/usuarioEstandar.h \
+    usuarioPremium.h \
     usuarioPremium/usuarioPremium.h
--- Codigo/desafio_2/main.cpp
+++ Codigo/desafio_2/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include "usuario.h"
+#include "usuario_estandar.h"
+// #include "usuarioPremium.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int opcion;
+    do {
+        cout << "1. Mostrar info usuario estandar\n";
+        cout << "2. Reproducir canción (estándar)\n";
+        cout << "3. Mostrar info usuario premium\n";
+        cout << "4. Gestionar favoritos (premium)\n";
+        cout << "0. Salir\n";
+        cin >> opcion;
+
+        switch(opcion) {
+        case 1: user1.mostrarInfo(); break;
+        case 2:
+            user1.incrementarReproduccion();
+            if(user1.mostrarPublicidad())
+                cout << "Anuncio!\n";
+            break;
+        case 3: user2.mostrarInfo(); break;
+        case 4: user2.agregarFavorito(...); break;
+        }
+
+    } while(opcion != 0);
+
 }
