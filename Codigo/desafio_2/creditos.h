cd15979b27695fd793a2b6c54858a0ee269f3cba
Jean <jean.agudelo@udea.edu.co>
2025-10-22 23:55:11 -0500

    xorrijo, en ves de sello, es codigo de sociedad, mi compañero me comento el error

--- Codigo/desafio_2/creditos.h
+++ Codigo/desafio_2/creditos.h
@@ -8,7 +8,7 @@ class creditos
 private:
     string nombre;
     string apellido;
-    string sello;
+    string codigoSociedad;
     string rol;
 
 public:
@@ -22,11 +22,11 @@ public:
 
     string getNombre() const;
     string getApellido() const;
-    string getSello() const;
+    string getcaodigoSociedad() const;
     string getRol() const;
     void setNombre(const string &n);
     void setApellido(const string &a);
-    void setSello(const string &s);
+    void setcodigoSociedad(const string &s);
     void setRol(const string &r);
 };
 
