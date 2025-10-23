3d845ef1e7878f486feceb5c51682cb4deb100fc
Jean <jean.agudelo@udea.edu.co>
2025-10-23 04:30:09 -0500

    empiezo con las funciones de las clases
    Signed-off-by: Jean <jean.agudelo@udea.edu.co>

--- Codigo/desafio_2/creditos.cpp
+++ Codigo/desafio_2/creditos.cpp
@@ -1,3 +1,30 @@
 #include "creditos.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 creditos::creditos() {}
+
+creditos::creditos(string nom, string ape, string cod, string rol):
+    nombre(nom),apellido(ape),codigoSociedad(cod),rol(r) {}
+
+creditos::creditos(const credito &c){
+    nombre = c.nombre;
+    apellido = c.apellido;
+    codigoSociedad = c.codigoSociedad;
+    rol = c.rol;
+}
+
+creditos::~Creditos() {}
+
+void creditos::mostrar() const{
+    cout<<rol<<": "<<nombre<<" "<<apellido<<" ("<<codigoSociedad<<")"<<endl;
+}
+
+string creditos::getNombre() const {return nombre;}
+string creditos::getApellido() const {return apellido;}
+string creditos::getcodigoSociedad() const {return codigoSociedad;}
+string creditos::getRol() const {return rol;}
+
+void creditos::setNombre(const string &n)
